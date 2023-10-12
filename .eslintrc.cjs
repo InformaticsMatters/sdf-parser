39c35ed05b03f43104e06e43e31746ae0cd62ee0
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2023-10-12 22:14:08 +0100

    chore: use v1 of squonk eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,3 @@
-// This is a patch so that eslint will load the plugins as dependencies.
-// Otherwise we can to install EVERYTHING in th root project
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 /**
  * @type {import("eslint").Linter.Config}
  */
