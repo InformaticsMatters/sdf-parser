9c928fd2cacc68108186c204dc459387e894b5a3
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2025-12-28 13:45:31 +0000

    chore: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 import config from "@squonk/eslint-config";
 
-export default config.map((c) => ({ ...c, ignores: ["**/dist/**"] }));
+export default config.map((c) => ({ ...c, ignores: ["**/dist/**"] })).slice(0, -1); // Remove the last rule which is for apps
